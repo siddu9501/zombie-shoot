135dee9892c30dffd5c9064355131402b8f91aa2
siddu9501 <siddusathu@yahoo.co.in>
2015-03-16 00:05:42 +0530

    added spotlight effects

--- Scripts/playerController.cs
+++ Scripts/playerController.cs
@@ -1,17 +1,51 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class playerController : MonoBehaviour {
-
-	public float movementSpeed = 10;
-	public float turningSpeed = 60;
-	
-	void Update() {
-		float horizontal = Input.GetAxis("Horizontal") * turningSpeed * Time.deltaTime;
-		transform.Rotate(0, horizontal, 0);
-		
-		float vertical = Input.GetAxis("Vertical") * movementSpeed * Time.deltaTime;
-		transform.Translate(0, 0, vertical);
+	public float speed;
+	public Light spotLight;
+	public GameObject trigger;
+	private Vector3 distance;
+	private double total_distance;
+	private double prev_total_distance;
+	private Vector3 prev_distance;
+	void Start()
+	{
 	}
+	void FixedUpdate()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+		rigidbody.AddForce (movement*speed*Time.deltaTime);
+	}
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag == "triggerLight") {
+			spotLight.intensity = spotLight.intensity + 1;
+			prev_distance = this.transform.position - other.gameObject.transform.position;
+			prev_total_distance = (prev_distance.x * prev_distance.x) + (prev_distance.z*prev_distance.z);
+		}
 
+	}
+	void onTriggerStay( Collider other)
+	{
+		if (other.gameObject.tag == "triggerLight") {
+			trigger = other.gameObject;
+		}
+		distance = this.transform.position - trigger.transform.position;
+		total_distance = (distance.x * distance.x) + (distance.z * distance.z);
+		if (total_distance < prev_total_distance) {
+			spotLight.intensity += 1.3f;
+			prev_total_distance = total_distance;
+				}
+		else if (total_distance > prev_total_distance) {
+						spotLight.intensity -= 1.3f;
+						prev_total_distance = total_distance;
+				}
+	}
+	void OnTriggerExit(Collider other) {
+		if (other.gameObject.tag == "triggerLight")
+						spotLight.intensity = 0.1f;
+	}
+	
 }
