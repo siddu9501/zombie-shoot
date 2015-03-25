76fea3f3dd919be553d90a7485e9495cbfd5924f
siddu9501 <siddusathu@yahoo.co.in>
2015-03-25 22:23:39 +0530

    Revert "Revert "Revert "Revert "added scoring, high scores""""
    This reverts commit e140d5ea093e2fa07501d3c767f27e7ff1eeb89a.

---
+++ Scripts/ZombieSpawn.js
@@ -0,0 +1,44 @@
+﻿#pragma strict
+var zombie :Transform;
+var spawn1 :Transform;
+var spawn2 :Transform;
+var spawn3 :Transform;
+var temp: float;
+
+var score:GUIText;
+var count = 0;
+function Start () {
+
+InvokeRepeating("zombieSpawnage",0,2);
+
+}
+function countreturn()
+{
+return count;
+}
+function Update () {
+
+score.text = "score: " + count.ToString();
+
+}
+function  zombieSpawnage()
+{
+  	temp =Random.value;
+  	Debug.Log(temp);
+  	if (temp >= 0.3 && temp < 0.6)
+	{
+		Instantiate(zombie,spawn1.position, Quaternion.identity);
+//		Debug.Log("spawn");
+	}
+	else if (temp >= 0.6 && temp < 0.9)
+	{
+		Instantiate(zombie,spawn2.position, Quaternion.identity);
+//		Debug.Log("spawn");
+	}	
+	else if (temp < 0.3 )
+	{
+		Instantiate(zombie,spawn3.position , Quaternion.identity);
+		//Debug.Log("spawn");
+	}
+ }
+  
