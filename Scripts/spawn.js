85ce5a60a9ab5914006d22728df21a573753f7b8
siddu9501 <siddusathu@yahoo.co.in>
2015-04-13 19:20:35 +0530

    13-4-2015
    added save feature.

---
+++ Scripts/spawn.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+
+
+function Start () {
+
+var currentPos = PlayerPrefs.GetInt("waypoint");
+var currentPositionString = currentPos.ToString();
+var current_waypoint = GameObject.FindGameObjectWithTag(currentPositionString);
+Debug.Log(currentPositionString);
+this.transform.position = current_waypoint.transform.position;
+
+}
+
+function Update () {
+
+
+}
+function OnTriggerEnter(other: Collider )
+{
+other.GetComponent(waypoint).collisionOccured();
+
+}
---
+++ Scripts/waypoint.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+var waypointnum:int;
+function Start () {
+
+}
+function collisionOccured()
+{
+
+PlayerPrefs.SetInt("waypoint", waypointnum);
+}
+
+function Update () {
+
+}
